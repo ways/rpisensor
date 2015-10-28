31c1cdbc09781719289fc6c230f8cbb53f767017
Lars <dev@falk-petersen.no>
2015-10-28 07:39:59 +0100

    Small bugs.

--- publish.py
+++ publish.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-import time
-import os				# os.sys.exit
+import time                             # Time calculations
+import os                               # os.sys.exit
 import socket                           # hostname
-import yaml				# config
+import yaml                             # config
 import paho.mqtt.publish as publish     # mosquitto
 import RPi.GPIO as GPIO                 # gpio setup
 GPIO.setmode(GPIO.BCM)
 
+#Hostname is used in topic
 hostname=socket.gethostname()
 
 # Load config
@@ -17,11 +18,12 @@ stream.close()
 
 mosquittoserver=config['mqtt']['broker']
 verbose=config['verbose']
-idle_delay=config['delay']['max_idle_time']
+max_idle_time=config['delay']['max_idle_time']
 activity_delay=1
 delay=activity_delay
 
 # Functions
+
 
 def getReading ():
   x,y,z = XLoBorg.ReadAccelerometer()
@@ -29,6 +31,7 @@ def getReading ():
   #if verbose:
   #  print "Raw %s %s %s, abs %s, summed %s." % (x, y, z, product, x+y+z)
   return product
+
 
 def updateProduct ():
   # Read several times to get a precise value.
@@ -68,21 +71,26 @@ for sensor in config['sensors']:
     GPIO.setup(config['sensors'][sensor]['gpio'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
     from w1thermsensor import W1ThermSensor # w1 temp
 
-  if 'xloborg' == config['sensors'][sensor]['type']:
+  elif 'xloborg' == config['sensors'][sensor]['type']:
     if verbose:
       print "Initializing %s. Hope you've activated i2c." % config['sensors'][sensor]['type']
     import XLoBorg
     XLoBorg.printFunction = XLoBorg.NoPrint
     XLoBorg.Init()
 
-  if 'pir' == config['sensors'][sensor]['type'] or 'reed' == config['sensors'][sensor]['type']:
+  elif 'pir' == config['sensors'][sensor]['type'] or 'reed' == config['sensors'][sensor]['type']:
     if verbose:
-      print "Initializing %s." % config['sensors'][sensor]['type']
+      print "Initializing %s on gpio %s." % (config['sensors'][sensor]['type'], config['sensors'][sensor]['gpio'])
     GPIO.setup(config['sensors'][sensor]['gpio'], GPIO.IN)
 
-state={} # state[gpio]=input
-last_change={} # last_change[gpio]=time
-last_report_time=0
+  else:
+    print "Error: wrong type of sensor in config? <%s>" % type
+    os.sys.exit(1)
+
+   
+state={}           # Keep track of states of sensors: state[gpio]=input
+last_change={}     # When a certain sensor change was last sent: last_change[gpio]=time
+last_report_time=0 # When any change was sent
 changed=False
 
 # Main loop
@@ -120,7 +128,8 @@ while True:
           else:
             changed=False
             if verbose:
-              print "Not time to send changed %s %s yet. Delay: %s. Remaining: %.0f." % (w1.id, input, delay, str(time.time()-last_change[w1.id]))
+              print "Not time to send changed %s %s yet. Delay: %s. Since last update: %.0f." \
+                % (w1.id, input, delay, (time.time()-last_change[w1.id]))
 
     elif 'xloborg' == type:
       product = updateProduct ()
@@ -142,15 +151,17 @@ while True:
       input='dummy test value'
       changed=True
 
-    else:
-      print "Error: wrong type of sensor in config? <%s>" % type
-      os.sys.exit(1)
+#    else:
+#      print "Error: wrong type of sensor in config? <%s>" % type
+#      os.sys.exit(1)
 
     # Common for all sensors except ds18b20
-    if changed and 'ds18b20' != type:
-      if (time.time()-last_change[gpio] > delay):
+    if 'ds18b20' != type:
+      # Check if changed and above delay for the sensor, or above max_idle_time
+      if changed and (time.time()-last_change[gpio] > delay) \
+        or (time.time()-last_change[gpio] > max_idle_time):
         if verbose:
-          print "Changed", gpio, input
+          print "Changed (or max_idle_time hit) for %s: %s" % (gpio, input)
         state[gpio] = input
         last_change[gpio] = time.time()
         messages.append({
@@ -159,8 +170,9 @@ while True:
       else:
         changed=False
         if verbose:
-          print "Not time to send %s, %s yet. Delay: %s. Remaining: %.0f" % (gpio, input, delay, str(time.time()-last_change[gpio]))
-
+          print "Not time to send %s: %s yet. Delay: %s. Since last update: %.0f." \
+            % (gpio, input, delay, (time.time()-last_change[gpio]))
+    
   # Send all
   if changed:
     if verbose:
