184edc432561defe3f711b596be1f16c46f64442
Lars <dev@falk-petersen.no>
2015-12-02 22:48:02 +0100

    Not working as it should...

--- rpisensor.py
+++ rpisensor.py
@@ -15,7 +15,7 @@ except ImportError as e:
 except RuntimeError as e:
   print "Error importing modules:", e
 
-sys_version=0.5
+sys_version=0.6
 
 #Hostname is used in topic
 hostname=socket.gethostname()
@@ -32,7 +32,7 @@ stream.close()
 
 mosquittoserver=config['mqtt']['broker']
 max_idle_time=config['delay']['max_idle_time']
-activity_delay=1
+activity_delay=2
 delay=activity_delay
 
 # Logging
@@ -84,6 +84,13 @@ def updateProduct ():
   return product
 
 
+def append_message(messages, topic, payload, changed):
+  messages.append({
+    'topic': topic,
+    'payload': payload})
+  changed=True
+
+
 # Initialize sensor setups
 
 if 1 > len(config['sensors']):
@@ -108,7 +115,6 @@ for sensor in config['sensors']:
     GPIO.setup(config['sensors'][sensor]['gpio'], GPIO.IN)
   
   elif 'dummy' == config['sensors'][sensor]['type']:
-    #logger.info ("Initializing %s on gpio %s." % (config['sensors'][sensor]['type'], config['sensors'][sensor]['gpio']))
     pass
 
   else:
@@ -118,16 +124,15 @@ for sensor in config['sensors']:
    
 state={}           # Keep track of states of sensors: state[gpio]=input
 last_change={}     # When a certain sensor change was last sent: last_change[gpio]=time
-last_report_time=0 # When any change was sent
-changed=False
 
 # Main loop
 
 while True:
   messages=[]
-  logger.debug ("time since prev update %.0f" % (time.time()-last_report_time))
+  changed=False
   
   for sensor in config['sensors']:
+    # Gather settings
     input=None
     type=config['sensors'][sensor]['type']
     gpio=config['sensors'][sensor]['gpio']
@@ -137,7 +142,6 @@ while True:
       delay=1
     try:
       invert=config['sensors'][sensor]['invert']
-      #logger.debug("Sensor %s is inverted." % config['sensors'][sensor]['gpio'])
     except KeyError:
       invert=False
     try:
@@ -145,19 +149,23 @@ while True:
     except KeyError:
       offset=0
 
-    # Set a default value just to fill last_change
-    if gpio not in last_change:
-      last_change[gpio]=99999
-
     if 'ds18b20' == type:
+      logger.debug("Reading %s" % type)
       for count, w1 in enumerate(W1ThermSensor.get_available_sensors()):
+        # Make sure a new reading will be fetched
         if w1.id not in last_change:
-          last_change[w1.id]=99999
-        try:
-          input = float("%.1f" % w1.get_temperature()) + offset
-        except W1ThermSensorError:
-        #except w1thermsensor.core.SensorNotReadyError:
-          logger.error("Unable to read %s sensor on gpio %." % (type, gpio))
+          last_change[w1.id]=time.time()-delay
+
+        # Read from sensor
+        #try:
+        input = float("%.1f" % w1.get_temperature()) + offset
+        #except W1ThermSensorError:
+        #  logger.error("Unable to read %s sensor on gpio %." % (type, gpio))
+        #  continue
+
+        if (input is None):
+          logger.error("Sensor %s gave invalid data %s." % (w1.id, input))
+          state[w1.id] = input
           continue
 
         # Store, but don't send first reading
@@ -165,22 +173,33 @@ while True:
           state[w1.id] = input
           continue
 
+        # Check if value is different from last reading
         if (input != state[w1.id]):
           changed=True
-          logger.debug ("Changed sensor %s, %s." % (w1.id, input))
-          if (time.time()-last_change[w1.id] > delay):
+          logger.debug ("Sensor %s changed from %s to %s." % (w1.id, state[w1.id], input))
+
+          # Check if we're allowed to send yet ( sensor/delay )
+          if changed and (time.time()-last_change[w1.id] > delay):
             state[w1.id] = input
             last_change[w1.id] = time.time()
-            messages.append({
-              'topic': hostname + '/' + type + '_' + str(count),
-              'payload': input})
-          else:
-            changed=False
-            logger.debug (
-              "Not time to send changed %s %s yet. Delay: %s. Since last update: %.0f."
-              % (w1.id, input, delay, (time.time()-last_change[w1.id])))
+            changed=True
+            append_message(messages, hostname + '/' + type + '_' + str(count), input, changed)
+
+        # Check if we should send anyway because prev value is older than max_idle_time
+        elif (time.time()-last_change[w1.id] > max_idle_time):
+            state[w1.id] = input
+            last_change[w1.id] = time.time()
+            changed=True
+            append_message(messages, hostname + '/' + type + '_' + str(count), input, changed)
+
+        # Do not send
+        else:
+          logger.debug (
+            "(Not sending %s yet. Delay: %s. Max idle: %s. Since last update of last_change[%s]: %.0f.)"
+            % (input, delay, max_idle_time, w1.id, (time.time()-last_change[w1.id])))
 
     elif type in ['digital', 'pir', 'reed']:
+      logger.debug("Reading %s" % type)
       if 'digital' == type:
         if not invert:
 	  input='1' if 1 == GPIO.input(gpio) else '0'
@@ -197,6 +216,7 @@ while True:
         logger.debug("%s changed to %s" % (gpio, input))
 
     elif 'xloborg' == type:
+      logger.debug("Reading %s" % type)
       product = updateProduct ()
       input = float( '%01.2f' % (product + offset) )
       
@@ -204,24 +224,38 @@ while True:
         changed=True
 
     elif 'dummy' == type:
+      logger.debug("Reading %s" % type)
       input=int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
-      changed=True
+      #changed=True
 
     # Common for all sensors except ds18b20
     if 'ds18b20' != type:
-      # Check if changed and above delay for the sensor, or above max_idle_time
-      if (changed and (time.time()-last_change[gpio] > delay) ) \
-        or (time.time()-last_change[gpio] > max_idle_time):
-        logger.info("Changed (or max_idle_time hit) for %s: %s" % (gpio, input))
+      # Skip first reading:
+      if gpio not in state:
+        state[gpio] = input
+        continue
+
+      # Set a default value just to fill last_change
+      if gpio not in last_change:
+        last_change[gpio]=99999
+
+      # Check if changed and above delay for the sensor
+      if (changed and (time.time()-last_change[gpio] > delay) ):
+        logger.info("Sending %s %s." % (gpio, input))
         state[gpio] = input
         last_change[gpio] = time.time()
-        messages.append({
-          'topic': (hostname + '/' + type + str(gpio)),
-          'payload': input})
-      else:
-        logger.debug("Changed: %s, or not time to send %s: %s yet. Delay: %s. Since last update: %.0f." \
-            % (changed, gpio, input, delay, (time.time()-last_change[gpio])))
-        changed=False
+        append_message(messages, hostname + '/' + type + str(gpio), input, changed)
+
+      # Check if above max_idle_time
+      if (time.time()-last_change[gpio] > max_idle_time):
+        logger.info("Max_idle_time hit for %s: %s" % (gpio, input))
+        state[gpio] = input
+        last_change[gpio] = time.time()
+        append_message(messages, hostname + '/' + type + str(gpio), input, changed)
+        continue
+      #else:
+      #  logger.debug("Not time to send %s yet. Delay: %s. Max idle: %s. Since last update of last_change[%s]: %.0f." \
+      #      % (input, delay, max_idle_time, gpio, (time.time()-last_change[gpio])))
   
   # Send all
   if changed:
@@ -230,7 +264,6 @@ while True:
     try:
       publish.multiple(messages, hostname=mosquittoserver, port=1883, client_id="", keepalive=60)
       changed=False
-      last_report_time=time.time()
     except Exception as err:
       logger.error("*** Error sending message *** %s." % err)
 
